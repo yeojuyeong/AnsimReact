012269be8c6d136bd9d9e857baa05313ab516c96
JongBell <jongh2107@gmail.com>
2024-01-03 16:19:16 +0900

    24-01-03 동행 신청 거절시 리스트 삭제

--- src/components/BoardView.js
+++ src/components/BoardView.js
@@ -42,59 +42,57 @@ const BoardView = () => {
     const [next_seqno, setNext_seqno] = useState(0);
 
     useEffect(()=> {
+        fetchData();
+    },[page,seqno,keyword]);
 
-        const fetchData = async() => {
+    const fetchData = async() => {
 
-            //게시물 상세 보기
-            const response = await axios.get(`http://localhost:8080/restapi/view?seqno=${seqno}&page=${page}&keyword=${keyword}&user_id=${cookie_user_id}`);
-            const data = response.data;
+        //게시물 상세 보기
+        const response = await axios.get(`http://localhost:8080/restapi/view?seqno=${seqno}&page=${page}&keyword=${keyword}&user_id=${cookie_user_id}`);
+        const data = response.data;
 
-            setUser_id(data.view.user_id);
-            setCookie_stored_file_nm(data.cookie_stored_file_nm);
-            setRole(data.role);
-            setTitle(data.view.title);
-            // setMap(view.data.map);
-            setDeparture(data.view.departure);
-            setDestination(data.view.destination);
-            setMeeting_time(data.view.meeting_time);
-            setMem_cnt(data.view.mem_cnt);
-            setGender(data.view.gender);
-            setSound(data.view.sound);
-            setStored_file_nm(data.view.stored_file_nm);
-            setUser_nm(data.view.user_nm);
-            setAge(data.view.age);
-            setMbti(data.view.mbti);
-            setMem_gender(data.view.mem_gender);
-            setAnsim_cnt(data.view.ansim_cnt);
-            setContent(data.view.content);
+        setUser_id(data.view.user_id);
+        setCookie_stored_file_nm(data.cookie_stored_file_nm);
+        setRole(data.role);
+        setTitle(data.view.title);
+        // setMap(view.data.map);
+        setDeparture(data.view.departure);
+        setDestination(data.view.destination);
+        setMeeting_time(data.view.meeting_time);
+        setMem_cnt(data.view.mem_cnt);
+        setGender(data.view.gender);
+        setSound(data.view.sound);
+        setStored_file_nm(data.view.stored_file_nm);
+        setUser_nm(data.view.user_nm);
+        setAge(data.view.age);
+        setMbti(data.view.mbti);
+        setMem_gender(data.view.mem_gender);
+        setAnsim_cnt(data.view.ansim_cnt);
+        setContent(data.view.content);
 
-            setPre_seqno(data.pre_seqno);
-            setNext_seqno(data.next_seqno);
-            setList(data.applicant_list);
+        setPre_seqno(data.pre_seqno);
+        setNext_seqno(data.next_seqno);
+        setList(data.applicant_list);
 
-            if (!cookie_user_id) {
-                alert('서비스 이용을 위해 로그인해주세요.');
-                window.location.href = 'http://localhost:3000/Login';  // 회원 정보 변경 페이지로
-            }
-
-            if (!data.cookie_stored_file_nm || data.cookie_stored_file_nm === 'null' || data.cookie_stored_file_nm === '') {
-                alert('프로필 사진을 먼저 등록해주세요.');
-                window.location.href = 'http://localhost:3000/Mypage';  // 회원 정보 변경 페이지로
-            }
-
-            // //이전 보기
-            // (이거... 굳이 필요한가? 맨 처음,끝에서 다음, 이전 보기
-            // 안 보이게 하려면 필요할 것 같기도 하고... 시간 없으니 일단 패스)
-            // const preseqno = await axios.get(`http://localhost:8080/restapi/preseqno?seqno=${seqno}&keyword=${keyword}`);
-            // setPre_seqno(data.pre_seqno);
-            // //다음 보기
-            // const nextseqno = await axios.get(`http://localhost:8080/restapi/nextseqno?seqno=${seqno}&keyword=${keyword}`);
-            // setNext_seqno(data.next_seqno);
+        if (!cookie_user_id) {
+            alert('서비스 이용을 위해 로그인해주세요.');
+            window.location.href = 'http://localhost:3000/Login';  // 회원 정보 변경 페이지로
         }
 
-        fetchData();
+        if (!data.cookie_stored_file_nm || data.cookie_stored_file_nm === 'null' || data.cookie_stored_file_nm === '') {
+            alert('프로필 사진을 먼저 등록해주세요.');
+            window.location.href = 'http://localhost:3000/Mypage';  // 회원 정보 변경 페이지로
+        }
 
-    },[page,seqno,keyword]);
+        // //이전 보기
+        // (이거... 굳이 필요한가? 맨 처음,끝에서 다음, 이전 보기
+        // 안 보이게 하려면 필요할 것 같기도 하고... 시간 없으니 일단 패스)
+        // const preseqno = await axios.get(`http://localhost:8080/restapi/preseqno?seqno=${seqno}&keyword=${keyword}`);
+        // setPre_seqno(data.pre_seqno);
+        // //다음 보기
+        // const nextseqno = await axios.get(`http://localhost:8080/restapi/nextseqno?seqno=${seqno}&keyword=${keyword}`);
+        // setNext_seqno(data.next_seqno);
+    }
 
     //게시물 삭제
     const boardDelete = () => {
@@ -159,6 +157,7 @@ const BoardView = () => {
             .then((data) => {
                 if(data.message === 'GOOD') {
                     alert('동행 신청을 거절 하였습니다.')
+                    fetchData();
                 } else if(data.message === 'CLICKED') {
                     alert('이미 거절한 멤버입니다.');
                 }
@@ -279,13 +278,13 @@ const BoardView = () => {
             <br />
                 <div className="view_bottom_menu">
                     {
-                        pre_seqno !== '0' && <Link to ={`/board/view?seqno=${pre_seqno}&page=${page}&keyword=${keyword}&user_id=${cookie_user_id}`}>이전글▼</Link>
+                        pre_seqno !== 0 && <Link to ={`/board/view?seqno=${pre_seqno}&page=${page}&keyword=${keyword}&user_id=${cookie_user_id}`}>이전글▼</Link>
                     }
                     &nbsp;&nbsp;
                     <Link to={`/board/list?page=${page}&keyword=${keyword}`}>목록보기</Link>
                     &nbsp;&nbsp;
                     {
-                        next_seqno !== '0' && <Link to={`/board/view?seqno=${next_seqno}&page=${page}&keyword=${keyword}&user_id=${cookie_user_id}`}>다음글▲</Link>
+                        next_seqno !== 0 && <Link to={`/board/view?seqno=${next_seqno}&page=${page}&keyword=${keyword}&user_id=${cookie_user_id}`}>다음글▲</Link>
                     }
                     &nbsp;&nbsp;
                     <a href="/board/write">글 작성</a>
